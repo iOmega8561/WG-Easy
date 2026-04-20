55e5d9dfa46707a8c05f9a52ea7e4f91e3c2a401
iOmega8561 <giusepperocco38@gmail.com>
2026-04-21 00:28:10 +0200

    Replaced tailwind.config.ts with mjs version (CommonJS)

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -1,9 +1,11 @@
-import type { Config } from 'tailwindcss'
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./www/index.html",
     "./www/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-} satisfies Config
+}
