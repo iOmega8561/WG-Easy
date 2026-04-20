2296f4d06f886283841c073e605185236ca89ca4
iOmega8561 <giusepperocco38@gmail.com>
2026-04-20 22:57:45 +0200

    Created Login form view

---
+++ src/www/view/Login.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { translate } from "../data/Translator";
+
+import Props from "../data/Props";
+import Api from "../data/Api";
+
+const Login: React.FC<Props.Login> = ({
+  setAuthenticated
+}) => {
+
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e: React.FormEvent, pw: string) => {
+    e.preventDefault();
+
+    Api.createSession(pw)
+      .then(() => {
+        setAuthenticated(true);
+      })
+  };
+
+  return (
+    <div className="
+    min-h-screen flex
+    bg-gray-50 dark:bg-neutral-800 
+    items-center justify-center">
+      <form onSubmit={(e) => {handleLogin(e, password)}} className="
+      bg-white dark:bg-neutral-700 
+      p-8 rounded-lg 
+      shadow-md w-80">
+        
+        <h1 className="
+        text-3xl font-medium dark:text-white
+        mb-6 text-center">
+          WireGuard
+        </h1>
+        
+        <input type="password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder={translate('password')}
+        className="
+        w-full p-2 mb-4 
+        border rounded dark:border-neutral-600 
+        dark:bg-neutral-800 dark:text-white"/>
+
+        <button type="submit" className="
+        w-full p-2
+        bg-red-800 hover:bg-red-700
+        text-white
+        rounded 
+        transition">
+          {translate('signIn')}
+        </button>
+
+      </form>
+    </div>
+  );
+}
+
+export default Login;
