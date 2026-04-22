10c6145dfbf0549683b3ff0c6090299ef3b1b747
iOmega8561 <giusepperocco38@gmail.com>
2026-04-22 16:16:37 +0200

    Moved response interfaces to Response.ts under namespace

--- src/www/data/Api.ts
+++ src/www/data/Api.ts
@@ -2,15 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 import Client from './Client';
-
-export interface SessionResponse {
-  requiresPassword: boolean;
-  authenticated: boolean;
-}
-
-export interface SuccessResponse {
-  success: boolean;
-}
+import Response from './Response';
 
 const apiClient = axios.create({
   baseURL: '/api',
@@ -33,34 +25,34 @@ const Api = {
   getRelease: (): Promise<string> => 
     apiClient.get('/release').then(res => res.data),
 
-  getSession: (): Promise<SessionResponse> => 
+  getSession: (): Promise<Response.Session> => 
     apiClient.get('/session').then(res => res.data),
 
-  createSession: (password: string): Promise<SuccessResponse> => 
+  createSession: (password: string): Promise<Response.Success> => 
     apiClient.post('/session', { password }).then(res => res.data),
 
-  deleteSession: (): Promise<SuccessResponse> => 
+  deleteSession: (): Promise<Response.Success> => 
     apiClient.delete('/session').then(res => res.data),
   
   getClients: (): Promise<Client[]> => 
     apiClient.get('/wireguard/client').then(res => res.data),
 
-  createClient: (name: string): Promise<SuccessResponse> => 
+  createClient: (name: string): Promise<Response.Success> => 
     apiClient.post('/wireguard/client', { name }).then(res => res.data),
 
-  deleteClient: (clientId: string): Promise<SuccessResponse> => 
+  deleteClient: (clientId: string): Promise<Response.Success> => 
     apiClient.delete(`/wireguard/client/${clientId}`).then(res => res.data),
 
-  enableClient: (clientId: string): Promise<SuccessResponse> => 
+  enableClient: (clientId: string): Promise<Response.Success> => 
     apiClient.post(`/wireguard/client/${clientId}/enable`).then(res => res.data),
 
-  disableClient: (clientId: string): Promise<SuccessResponse> => 
+  disableClient: (clientId: string): Promise<Response.Success> => 
     apiClient.post(`/wireguard/client/${clientId}/disable`).then(res => res.data),
 
-  updateClientName: (clientId: string, name: string): Promise<SuccessResponse> => 
+  updateClientName: (clientId: string, name: string): Promise<Response.Success> => 
     apiClient.put(`/wireguard/client/${clientId}/name`, { name }).then(res => res.data),
 
-  updateClientAddress: (clientId: string, address: string): Promise<SuccessResponse> => 
+  updateClientAddress: (clientId: string, address: string): Promise<Response.Success> => 
     apiClient.put(`/wireguard/client/${clientId}/address`, { address }).then(res => res.data),
 
   getClientQRCode: (clientId: string): Promise<string> => 
@@ -75,7 +67,7 @@ const Api = {
     window.location.href = `/api/wireguard/backup`;
   },
 
-  restoreConfiguration: async (file: File): Promise<SuccessResponse> => {
+  restoreConfiguration: async (file: File): Promise<Response.Success> => {
     return apiClient.put('/wireguard/restore', { file: await file.text() }).then(res => res.data);
   },
 };
