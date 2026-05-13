4f27e32b0296d212fdc30e8ad932e16c4b6f22cd
iOmega8561 <giusepperocco38@gmail.com>
2026-05-13 23:32:50 +0200

    Created "Utility" namespace with function useIsDark

---
+++ src/www/data/Utility.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+namespace Utility {
+
+  export function useIsDark(): string {
+    const [isDark, setIsDark] = useState(() =>
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+
+    useEffect(() => {
+      const mql = window.matchMedia('(prefers-color-scheme: dark)')
+      const handler = (e: MediaQueryListEvent) => setIsDark(e.matches)
+      mql.addEventListener('change', handler)
+      return () => mql.removeEventListener('change', handler)
+    }, [])
+
+    return isDark ? 'dark' : 'light'
+  }
+}
+
+export default Utility;
