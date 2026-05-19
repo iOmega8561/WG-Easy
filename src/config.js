b76658888fb174b8c08566d30af68e9a8908c29c
iOmega8561 <giusepperocco38@gmail.com>
2026-05-19 23:59:53 +0200

    New ENV var in config.js -- WG_MANAGED The idea here is that if the variable is set (don't matter the value), the backend will manage wireguard vpn

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ const { version } = require('./package.json');
 module.exports.RELEASE = version;
 module.exports.PORT = process.env.PORT || '3000';
 module.exports.WEBUI_HOST = process.env.WEBUI_HOST || '0.0.0.0';
-/** This is only kept for migration purpose. DO NOT USE! */
 module.exports.PASSWORD_HASH = process.env.PASSWORD_HASH;
 module.exports.WG_PATH = process.env.WG_PATH || '/etc/wireguard/';
 module.exports.WG_HOST = process.env.WG_HOST;
 module.exports.WG_PORT = process.env.WG_PORT || '51820';
 module.exports.WG_CONFIG_PORT = process.env.WG_CONFIG_PORT || process.env.WG_PORT || '51820';
+module.exports.WG_MANAGED = process.env.WG_MANAGED || null;
 module.exports.WG_MTU = process.env.WG_MTU || null;
 module.exports.WG_PERSISTENT_KEEPALIVE = process.env.WG_PERSISTENT_KEEPALIVE || '0';
 module.exports.WG_DEFAULT_ADDRESS = process.env.WG_DEFAULT_ADDRESS || '10.8.0.x';
@@ -19,6 +19,6 @@ module.exports.WG_DEFAULT_DNS = typeof process.env.WG_DEFAULT_DNS === 'string'
   : '1.1.1.1';
 module.exports.WG_ALLOWED_IPS = process.env.WG_ALLOWED_IPS || '0.0.0.0/0, ::/0';
 module.exports.WG_PRE_UP = process.env.WG_PRE_UP || '';
-module.exports.WG_POST_UP = process.env.WG_POST_UP || '';
+module.exports.WG_POST_UP = process.env.WG_POST_UP || null;
 module.exports.WG_PRE_DOWN = process.env.WG_PRE_DOWN || '';
-module.exports.WG_POST_DOWN = process.env.WG_POST_DOWN || '';
+module.exports.WG_POST_DOWN = process.env.WG_POST_DOWN || null;
