410b0ae35ca2a8985069df9683d4105a091fde35
iOmega8561 <giusepperocco38@gmail.com>
2026-05-20 23:32:11 +0200

    Created function Utility.formatBytes -> string

--- src/www/data/Utility.ts
+++ src/www/data/Utility.ts
@@ -16,6 +16,15 @@ namespace Utility {
 
     return isDark ? 'dark' : 'light'
   }
+
+  export function formatBytes (bytes: number, decimals = 2): string {
+    if (bytes === 0 || !bytes) return '0 B';
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+  };
 }
 
 export default Utility;
