f4afee5b60aef590ed6173ef5800f5eda9c92cbe
iOmega8561 <giusepperocco38@gmail.com>
2026-05-23 16:32:32 +0200

    refactor: updated project files switching to type "module"
    Updated package.json's sections type, scripts.
    Type has been set to "module", while "scripts" has been rewritten to accomodate a typescript backend that will sit under src/lib
    
    Furthermore, the outputs will be located under src/dist/lib and src/dist/www respectively for backend and frontend
    
    Renamed tailwind.config.mjs -> tailwind.config.ts, updated import/export syntax
    
    updated vite.config.ts to use the new location src/dist/www for the outputs (used to be src/www/dist)
    
    updated tsconfig.json, with "module": "esnext", "moduleResolution": "bundler"
    
    Removed package express-session, added package tsup along with @types/* for packages used by the backend

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import backendConfig from './config.js';
+import backendConfig from './lib/config/env';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
